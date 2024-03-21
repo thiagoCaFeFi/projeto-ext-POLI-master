85b6558b51d7d41060f0fe93aac56db95200b336
Emerson Nascimento <you@example.com>
2024-03-21 02:18:21 -0300

    atualizacao4

--- src/pages/Med/Medlogado.jsx
+++ src/pages/Med/Medlogado.jsx
@@ -1,11 +1,22 @@
 import React from 'react';
+import './login.css'
+import Header2 from "../../components/header2.jsx"
 
 function Medlogado() {
     return (
         <div>
-            <h1>Página Médico Logado</h1>
+            <div>
+            <Header2/>
+            </div>
+        <div className="title">
+            <span>Resultados de exames mais recentes</span>
         </div>
+        <div className='Exames-container'>
+            <h1 className='dentro'>roi</h1>
+        </div>
+    </div>
     );
 }
 
 export default Medlogado;
+
